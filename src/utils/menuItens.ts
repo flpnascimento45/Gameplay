8e1fbb3889c7cc296d0ab4e479a475fc64835c2b
flpnascimento45 <flpnascimento45@gmail.com>
2021-06-25 15:45:08 -0300

    Projeto original

---
+++ src/utils/menuItens.ts
@@ -0,0 +1,5 @@
+export const menuItens = [
+    { id: '1', title: 'Criar partida', icon: "plus", redir: "AppointmentCreate" },
+    { id: '2', title: 'Ranking amigos', icon: "crown", redir: "AppointmentCreate" },
+    { id: '3', title: 'Sair', icon: "presence-exit", redir: "AppointmentCreate" }
+]
